24cd8016e7d51c685042d14315ac15864949adeb
steve <steve@ABBITH>
2023-03-14 16:16:00 -0400

    Add project files.

---
+++ src/Argot/Argot.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Argot;
+
+public static class Argot
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
---
+++ src/Argot/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Argot;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => Argot.CreateMauiApp();
+}
